bdac27f23069e3a887284d0d6ca61b958a71f0a4
Ruslan Talpa <ruslan.talpa@gmail.com>
2022-05-03 10:27:53 +0300

    fix docker build scripts

--- scripts/docker-manifest-push.sh
+++ scripts/docker-manifest-push.sh
@@ -8,12 +8,12 @@ docker manifest rm subzerocloud/subzero:$VERSION
 docker manifest rm subzerocloud/subzero:latest
 
 docker manifest create subzerocloud/subzero:$VERSION \
-    --amend subzerocloud/subzero:$VERSION-amd64 \
-    --amend subzerocloud/subzero:$VERSION-arm64
+    subzerocloud/subzero:$VERSION-x86_64 \
+    subzerocloud/subzero:$VERSION-arm64
 
 docker manifest create subzerocloud/subzero:latest \
-    --amend subzerocloud/subzero:latest-amd64 \
-    --amend subzerocloud/subzero:latest-arm64
+    subzerocloud/subzero:latest-x86_64 \
+    subzerocloud/subzero:latest-arm64
 
 docker manifest push subzerocloud/subzero:$VERSION
 docker manifest push subzerocloud/subzero:latest
--- scripts/docker-push.sh
+++ scripts/docker-push.sh
@@ -3,5 +3,5 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 source $SCRIPT_DIR/.env
 
 echo "Pushing docker image for subzerocloud/subzero:$VERSION-$ARCH"
-docker push subzerocloud/subzero:$VERSION-arm64
-docker push subzerocloud/subzero:latest-arm64
+docker push subzerocloud/subzero:$VERSION-$ARCH
+docker push subzerocloud/subzero:latest-$ARCH
