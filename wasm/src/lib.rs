50aa5d9a9c8f05b3fac87ddf5b2f06be75e751a0
Ruslan Talpa <ruslan.talpa@gmail.com>
2022-07-28 14:33:43 +0300

    add support for all backends in wasm module

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,19 +1,34 @@
-mod utils;
+// #![no_std]
 
+mod utils;
+use std::collections::HashMap;
+
+use utils::{
+    set_panic_hook, 
+    cast_core_err, cast_serde_err,
+    //console_log, log,
+};
 //use subzero_core::schema::DbSchema;
 use subzero_core::{
     // api::{ ContentType, ContentType::*, Preferences, QueryNode::*, Representation, Resolution::*, SelectItem::*},
-    error::{Error, },
+    //error::{Error, },
     parser::postgrest::parse,
     schema::DbSchema,
-    formatter::{postgresql::{fmt_main_query, generate}, },
+    
 };
+#[cfg(feature = "postgresql")]
+use subzero_core::formatter::postgresql;
+#[cfg(feature = "clickhouse")]
+use subzero_core::formatter::clickhouse;
+#[cfg(feature = "sqlite")]
+use subzero_core::formatter::sqlite;
+
 // use snafu::Snafu;
 // use std::fmt;
 use wasm_bindgen::{prelude::*, };
-use js_sys::{JsString, Error as JsError, Array, Map};
+use js_sys::{Error as JsError, Array as JsArray, Map as JsMap, JsString};
 use serde_json;
-use std::{collections::HashMap, };
+//use std::{collections::HashMap, };
 use http::Method;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
@@ -23,92 +38,42 @@ use http::Method;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-extern "C" {
-    // Use `js_namespace` here to bind `console.log(..)` instead of just
-    // `log(..)`
-    #[wasm_bindgen(js_namespace = console)]
-    fn log(s: &str);
-
-    // The `console.log` is quite polymorphic, so we can bind it with multiple
-    // signatures. Note that we need to use `js_name` to ensure we always call
-    // `log` in JS.
-    #[wasm_bindgen(js_namespace = console, js_name = log)]
-    fn log_u32(a: u32);
-
-    // Multiple arguments too!
-    #[wasm_bindgen(js_namespace = console, js_name = log)]
-    fn log_many(a: &str, b: &str);
-}
-macro_rules! console_log {
-    // Note that this is using the `log` function imported above during
-    // `bare_bones`
-    ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
-}
-#[wasm_bindgen]
-extern {
-    fn alert(s: &str);
-}
-
-#[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, wasm!");
-}
-
-#[wasm_bindgen]
-#[derive(Debug,)]
-pub struct WasmError(Error);
-
-
-#[wasm_bindgen]
 pub struct Backend {
     db_schema: DbSchema
-}
-
-fn cast_core_err(err: Error) -> JsError {
-    let e = err.json_body();
-    JsError::new(e.get("message").unwrap().as_str().unwrap_or("internal error"))
-}
-
-fn cast_serde_err(err: serde_json::Error) -> JsError {
-    JsError::new(err.to_string().as_str())
 }
 
 #[wasm_bindgen]
 impl Backend {
     pub fn init(s: &str) -> Backend {
+        set_panic_hook();
         let db_schema = serde_json::from_str(s).expect("invalid schema json");
         Backend { db_schema }
     }
     pub fn get_query(
         &self,
-        schema_name: &JsString,
-        root: &JsString, 
-        method: &JsString, 
-        path: &JsString, 
-        get: &Array, 
-        body: Option<JsString>,
-        headers: &Map,
-        cookies: &Map,
+        schema_name: &str,
+        root: &str, 
+        method: &str, 
+        path: &str, 
+        get: &JsArray, 
+        //body: Option<JsString>,
+        body: &str,
+        headers: &JsMap,
+        cookies: &JsMap,
+        //db_type: Option<JsString>,
+        db_type: &str,
     )
-    -> Result<String, JsError> {
-        let schema_name: String = schema_name.into();
-        //console_log!("schema_name: {}", schema_name);
-        let root: String = root.into();
-        //console_log!("root: {}", root);
-        let method: String = method.into();
-        //console_log!("method: {}", method);
-        let path: String = path.into();
-        //console_log!("path: {}", path);
-        let get:Vec<(String, String)> = get.into_serde().map_err(cast_serde_err)?;
-        //console_log!("get: {:?}", get);
-        let body:Option<String> = body.map(|s| s.into());
-        //console_log!("body: {:?}", body);
-        let headers: HashMap<String, String> = headers.into_serde().map_err(cast_serde_err)?;
-        //console_log!("headers: {:?}", headers);
-        let cookies: HashMap<String, String> = cookies.into_serde().map_err(cast_serde_err)?;
-        //console_log!("cookies: {:?}", cookies);
+    -> Result<JsString, JsError> {
+        
+        //let body = body.into();
+        let get = get.into_serde::<Vec<(String,String)>>().map_err(cast_serde_err)?;
+        let get = get.iter().map(|(k,v)|(k.as_str(),v.as_str())).collect();
+        let headers = headers.into_serde::<HashMap<String,String>>().map_err(cast_serde_err)?;
+        let headers = headers.iter().map(|(k,v)|(k.as_str(),v.as_str())).collect();
+        let cookies = cookies.into_serde::<HashMap<String,String>>().map_err(cast_serde_err)?;
+        let cookies = cookies.iter().map(|(k,v)|(k.as_str(),v.as_str())).collect();
         let db_schema = &self.db_schema;
-        let method = match method.as_str() {
+        let method = match method {
             "get" => Method::GET,
             "post" => Method::POST,
             "put" => Method::PUT,
@@ -116,10 +81,34 @@ impl Backend {
             "patch" => Method::PATCH,
             _ => Method::GET,
         };
+        let body = if body.is_empty() {
+            None
+        } else {
+            Some(body)
+        };
         let max_rows = None;
-        let request = parse(&schema_name, &root, db_schema, &method, path, get, body, headers, cookies, max_rows).map_err(cast_core_err)?;
-        let query = fmt_main_query(&request.schema_name, &request).map_err(cast_core_err)?;
-        let (main_statement, _main_parameters, _) = generate(query);
-        Ok(main_statement)
+
+        let request = parse(schema_name, root, db_schema, &method, path, get, body, headers, cookies, max_rows).map_err(cast_core_err)?;
+
+        //let db_type = db_type.unwrap_or(JsString::from(""));
+        let (main_statement, _main_parameters, _) = match db_type {
+            "postgresql" => {
+                let query = postgresql::fmt_main_query(request.schema_name, &request).map_err(cast_core_err)?;
+                Ok(postgresql::generate(query))
+            },
+            "clickhouse" => {
+                let query = clickhouse::fmt_main_query(request.schema_name, &request).map_err(cast_core_err)?;
+                Ok(clickhouse::generate(query))
+            },
+            "sqlite" => {
+                let query = sqlite::fmt_main_query(request.schema_name, &request).map_err(cast_core_err)?;
+                Ok(sqlite::generate(query))
+            },
+            _ => Err(JsError::new("unsupported database type")),
+        }?;
+
+        Ok(JsString::from(main_statement))
     }
 }
+
+
--- wasm/src/utils.rs
+++ wasm/src/utils.rs
@@ -1,3 +1,6 @@
+use wasm_bindgen::{prelude::*, };
+use js_sys::{Error as JsError, };
+use subzero_core;
 pub fn set_panic_hook() {
     // When the `console_error_panic_hook` feature is enabled, we can call the
     // `set_panic_hook` function at least once during initialization, and then
@@ -7,4 +10,38 @@ pub fn set_panic_hook() {
     // https://github.com/rustwasm/console_error_panic_hook#readme
     #[cfg(feature = "console_error_panic_hook")]
     console_error_panic_hook::set_once();
+}
+
+#[wasm_bindgen]
+extern "C" {
+    // Use `js_namespace` here to bind `console.log(..)` instead of just
+    // `log(..)`
+    #[wasm_bindgen(js_namespace = console)]
+    pub fn log(s: &str);
+
+    // The `console.log` is quite polymorphic, so we can bind it with multiple
+    // signatures. Note that we need to use `js_name` to ensure we always call
+    // `log` in JS.
+    #[wasm_bindgen(js_namespace = console, js_name = log)]
+    pub fn log_u32(a: u32);
+
+    // Multiple arguments too!
+    #[wasm_bindgen(js_namespace = console, js_name = log)]
+    pub fn log_many(a: &str, b: &str);
+}
+#[allow(unused_macros)]
+macro_rules! console_log {
+    // Note that this is using the `log` function imported above during
+    // `bare_bones`
+    ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
+}
+#[allow(unused_imports)]
+pub(super) use console_log;
+
+pub fn cast_core_err(err: subzero_core::error::Error) -> JsError {
+    JsError::new(err.json_body().get("message").unwrap().as_str().unwrap_or("internal error"))
+}
+
+pub fn cast_serde_err(err: serde_json::Error) -> JsError {
+    JsError::new(err.to_string().as_str())
 }
