8692f4511998b8f35c999bcdb14e0f17f55d261c
Ruslan Talpa <ruslan.talpa@subzero.cloud>
2022-10-21 09:52:20 +0300

    fix formatting

--- subzero-wasm/tests/web.rs
+++ subzero-wasm/tests/web.rs
@@ -9,10 +9,7 @@ use serde_wasm_bindgen::to_value as to_js_value;
 use serde_json::json;
 
 // wasm_bindgen_test_configure!(run_in_browser);
-fn vs(v: Vec<(&str, &str)>) -> Vec<(String, String)> {
-    v.into_iter().map(|(s, s2)| (s.to_string(), s2.to_string())).collect()
-}
-
+fn vs(v: Vec<(&str, &str)>) -> Vec<(String, String)> { v.into_iter().map(|(s, s2)| (s.to_string(), s2.to_string())).collect() }
 
 #[wasm_bindgen_test]
 fn pass() {
@@ -22,12 +19,12 @@ fn pass() {
 #[wasm_bindgen_test]
 fn js_value_conversion() {
     assert_eq!(
-        vs(vec![("a","1"),("b","2")]),
+        vs(vec![("a", "1"), ("b", "2")]),
         from_js_value::<Vec<(String, String)>>(to_js_value(&json!({"a":"1","b":"2"})).unwrap()).unwrap()
     );
 
     assert_eq!(
-        vs(vec![("a","1"),("b","2")]),
-        from_js_value::<Vec<(String, String)>>(to_js_value(&json!([["a","1"],["b","2"]])).unwrap()).unwrap()
+        vs(vec![("a", "1"), ("b", "2")]),
+        from_js_value::<Vec<(String, String)>>(to_js_value(&json!([["a", "1"], ["b", "2"]])).unwrap()).unwrap()
     );
 }
